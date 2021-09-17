167f162a36580f3d2e49d18bb4b460c51c95dbb9
kal9338 <kal9338@nyu.edu>
2021-09-16 20:47:17 -0400

    modifications

--- solution.py
+++ solution.py
@@ -3,6 +3,8 @@
 ### Input - All eight questions given in the assignment.
 ### Output - The right answer for the specific question.
 
+question = " "
+
 def welcome_assignment_answers(question):
     #The student doesn't have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
