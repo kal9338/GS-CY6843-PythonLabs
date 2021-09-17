18e1548d3b80838c87b2e7b8bd9efa117165f183
kal9338 <kal9338@nyu.edu>
2021-09-16 21:26:53 -0400

    modifications

--- solution.py
+++ solution.py
@@ -3,13 +3,11 @@
 ### Input - All eight questions given in the assignment.
 ### Output - The right answer for the specific question.
 
-question = " "
-
 def welcome_assignment_answers(question):
     #The student doesn't have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
     if question == "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
-        answer = str("mTLS")
+        answer = "mTLS"
     if question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     if question == "Is it possible to decrypt a message without a key? - Yes/No":
@@ -22,9 +20,9 @@ def welcome_assignment_answers(question):
         answer = "68c74b1fe84ac8c662849915bc9002f4"
     if question ==  "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
-    if question == "What layer from the TCP/IP model the protocol DHCP belongs to?":
-        answer = 0
-    elif question ==  "What layer of the TCP/IP model the protocol TCP belongs to?":
+    if question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
+        answer = int(0)
+    elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
            answer = int(4)
     return(answer)
 # Complete all the questions.
